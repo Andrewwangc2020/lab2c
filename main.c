b8b6b2b47e2dcede546e8d9731e64536a08b082a
Andrewwangc2020 <Andrewwang.c2020@gmail.com>
2020-09-10 15:40:05 +0000

    included readline and stdlib

--- main.c
+++ main.c
@@ -6,27 +6,30 @@
 // Breakout: 7
 
 #include <stdio.h>
+#include <readline/readline.h>
+#include<stdlib.h>
 
 int main(void) {
-  float grade = scanf("Enter your CMPSC 131 grade:");
+  char *g = readline("Enter your CMPSC 131 grade:");
+  float grade = atof(g);
   char *letter = "F";
   if(grade >= 93.0){
     letter = "A";
-  } else if(grade >= 90.0){
+  } else if (grade >= 90.0){
     letter = "A-";
-  } else if(grade >= 87.0){
+  } else if (grade >= 87.0){
     letter = "B+";
-  } else if(grade >= 83.0){
+  } else if (grade >= 83.0){
     letter = "B";
-  } else if(grade >= 80.0){
+  } else if (grade >= 80.0){
     letter = "B-";
-  } else if(grade >= 77.0){
+  } else if (grade >= 77.0){
     letter = "C+";
-  } else if(grade >= 70.0){
+  } else if (grade >= 70.0){
     letter = "C";
-  } else if(grade >= 60.0){
+  } else if (grade >= 60.0){
     letter = "D";
-  } else if(grade < 60){
+  } else if (grade < 60){
     letter = "F";
   }
   printf("Your letter grade for CMPSC 131 is %s.",letter);
