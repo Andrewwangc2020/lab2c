19f142a9da1e74ec5580d9fad10413bdf4bfcdbc
Andrewwangc2020 <Andrewwang.c2020@gmail.com>
2020-09-10 15:27:04 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,34 @@
+// Author: Andrew Wang aqw5628@psu.edu
+// Collaborator: Chris Belt cob5463@psu.edu
+// Collaborator: Eric Beardslee eab6024@psu.edu
+// Collaborator: Devanshi Mittal dfm5688@psu.edu
+// Section: 005R
+// Breakout: 7
+
+#include <stdio.h>
+
+int main(void) {
+  float grade = scanf("Enter your CMPSC 131 grade:");
+  char *letter;
+  if(grade >= 93.0){
+    letter = "A";
+  } else if(grade >= 90.0){
+    letter = "A-";
+  } else if(grade >= 87.0){
+    letter = "B+";
+  } else if(grade >= 83.0){
+    letter = "B";
+  } else if(grade >= 80.0){
+    letter = "B-";
+  } else if(grade >= 77.0){
+    letter = "C+";
+  } else if(grade >= 70.0){
+    letter = "C";
+  } else if(grade >= 60.0){
+    letter = "D";
+  } else if(grade < 60){
+    letter = "F";
+  }
+  printf("Your letter grade for CMPSC 131 is %s.",letter);
+  return 0;
+}
